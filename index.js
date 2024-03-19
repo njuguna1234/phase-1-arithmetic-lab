e65f424cef90adb9df81b41227fe4b8c2d536ef8
roy wahome <roy.njuguna@student.moringaschool.com>
2024-03-19 12:16:28 +0300

    the math

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+let num1 =2
+let num2 =31
+let multiply=num1 * num2
+console.log (multiply)
 
-// Write your code here
+let random =45
+Number.isInteger(45)
+    
+let num3 =24
+let num4 =5
+let mod = num3%num4
+console.log (mod)
+
+let max= Math.max= (1,2,4,6,8,20)
+console.log(max)
+
+
+
+
